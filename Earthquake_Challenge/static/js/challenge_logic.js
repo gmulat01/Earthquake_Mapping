c243852680184a2f13d89ba5ef16a1c818d02dea
gmulat01 <102130439+gmulat01@users.noreply.github.com>
2022-08-27 13:59:07 -0500

    HW

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -28,11 +28,6 @@ function makeMap(features) {
       accessToken: API_KEY
       });
 
-    // make earthquake layer here
-    let earthquakeLayer = L.geoJSON(features, {
-      onEachFeature: onEachFeature
-    });
-
     // Create a new map
     let map = L.map("map", {
       center: [37.09, -95.71],
@@ -49,10 +44,12 @@ function makeMap(features) {
     //Add a 2nd layer group for the tectonic plate data.
     let allEarthquakes = new L.LayerGroup();
     let tectonicPlates = new L.LayerGroup();
+    let majorEarthquakes = new L.LayerGroup();
 
     let overlayMaps = {
       "Tectonic Plates": tectonicPlates,
-      "Earthquakes": earthquakeLayer
+      "Earthquakes": allEarthquakes,
+      "Major Earthquakes": majorEarthquakes
     };
 
   // Then we add a control to the map that will allow the user to change which
@@ -109,7 +106,7 @@ function makeMap(features) {
     // Creating a GeoJSON layer with the retrieved data.
     L.geoJson(data, {
         // We turn each feature into a circleMarker on the map.
-        pointToLayer: function(feature, latlng) {
+        pointToLayer: function(features, latlng) {
             console.log(data);
             return L.circleMarker(latlng);
           },
@@ -118,7 +115,7 @@ function makeMap(features) {
       // We create a popup for each circleMarker to display the magnitude and location of the earthquake
       //  after the marker has been created and styled.
       onEachFeature: function(features, layer) {
-        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+        layer.bindPopup("Magnitude: " + features.properties.mag + "<br>Location: " + features.properties.place);
       }
     }).addTo(allEarthquakes);
 
@@ -126,19 +123,19 @@ function makeMap(features) {
     allEarthquakes.addTo(map);
   
     // Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
-  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
-  // Use the same style as the earthquake data.
-  function styleInfo(feature) {
-    return {
-      opacity: 1,
-      fillOpacity: 1,
-      fillColor: getColor(feature.properties.mag),
-      color: "#000000",
-      radius: getRadius(feature.properties.mag),
-      stroke: true,
-      weight: 0.5
-    };
-  }
+    d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
+    // Use the same style as the earthquake data.
+    function styleInfo(feature) {
+      return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: getColor(feature.properties.mag),
+        color: "#000000",
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+      };
+    }
   // Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
   function getColor(magnitude) {
     if (magnitude > 6) {
@@ -168,7 +165,7 @@ function makeMap(features) {
   // We create a popup for each circleMarker to display the magnitude and location of the earthquake
   //  after the marker has been created and styled.
   onEachFeature: function(features, layer) {
-    layer.bindPopup("Magnitude: " + features.properties.mag + "<br>Location: " + feature.properties.place);
+    layer.bindPopup("Magnitude: " + features.properties.mag + "<br>Location: " + features.properties.place);
   }
   }).addTo(majorEarthquakes);
 
